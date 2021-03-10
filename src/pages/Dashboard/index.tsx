e82da8a9be0b74c195ad64efc408265e8542630c
Andre Dionisio <andredionisio12@gmail.com>
2021-03-09 23:30:08 -0300

    styling dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://github.com/adasantos.png" alt="André Santos" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy higly scalable</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://github.com/adasantos.png" alt="André Santos" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy higly scalable</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://github.com/adasantos.png" alt="André Santos" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy higly scalable</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
